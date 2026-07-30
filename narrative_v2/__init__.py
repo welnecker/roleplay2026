3f4ef08b85d7bf596aaf9e115f8740abdea01bab
welnecker <janiofdr@gmail.com>
2026-07-30 07:43:04 -0300

    Criar módulo do runtime narrativo v2

---
+++ narrative_v2/__init__.py
@@ -0,0 +1,19 @@
+from narrative_v2.models import (
+    BeatDefinition,
+    BlockDefinition,
+    CharacterProfile,
+    EndingResult,
+    NarrativePackage,
+    RunCredit,
+    StoryRun,
+)
+
+__all__ = [
+    "BeatDefinition",
+    "BlockDefinition",
+    "CharacterProfile",
+    "EndingResult",
+    "NarrativePackage",
+    "RunCredit",
+    "StoryRun",
+]
