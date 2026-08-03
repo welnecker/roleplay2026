139d65ac2cdeb6c58b3388e0746dc19dd3c234d4
welnecker <janiofdr@gmail.com>
2026-08-03 09:58:50 -0300

    Expor limpeza genérica da progressão editorial

--- services/editorial_progression.py
+++ services/editorial_progression.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from services.supermarket_script_v2 import (
     automatic_followups_after,
     classify_contextual_user_message,
+    clean_supermarket_script_v2_response,
     decide_supermarket_script_v2_turn,
     prepare_supermarket_script_v2,
     state_after_automatic_followup,
@@ -14,12 +15,14 @@ from services.supermarket_script_v2 import (
 prepare_editorial_script = prepare_supermarket_script_v2
 decide_editorial_progression_turn = decide_supermarket_script_v2_turn
 classify_contextual_editorial_message = classify_contextual_user_message
+clean_editorial_progression_response = clean_supermarket_script_v2_response
 editorial_followups_after = automatic_followups_after
 state_after_editorial_followup = state_after_automatic_followup
 
 
 __all__ = [
     "classify_contextual_editorial_message",
+    "clean_editorial_progression_response",
     "decide_editorial_progression_turn",
     "editorial_followups_after",
     "prepare_editorial_script",
