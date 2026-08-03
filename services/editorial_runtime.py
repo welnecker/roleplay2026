1457d2bb42c0095b2d3930878087c1de0d892fea
welnecker <janiofdr@gmail.com>
2026-08-03 15:37:32 -0300

    Usar contrato central dos tipos editoriais

--- services/editorial_runtime.py
+++ services/editorial_runtime.py
@@ -7,21 +7,19 @@ app e novos cards devem importar apenas deste módulo.
 """
 
 from services.editorial_runtime_impl import (
-    Engagement,
-    PilotScript,
-    PilotState,
-    PilotTurn,
     classify_user_message,
     clean_model_response,
     decide_turn,
     opening_text,
 )
+from services.editorial_runtime_types import (
+    EditorialEngagement,
+    EditorialScript,
+    EditorialState,
+    EditorialTurn,
+)
 
 
-EditorialEngagement = Engagement
-EditorialScript = PilotScript
-EditorialState = PilotState
-EditorialTurn = PilotTurn
 classify_editorial_user_message = classify_user_message
 decide_editorial_turn = decide_turn
 clean_editorial_model_response = clean_model_response
