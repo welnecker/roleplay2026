9a1133a73a0d6394df714847fca6ceff05226b2f
welnecker <janiofdr@gmail.com>
2026-08-03 15:04:20 -0300

    Remover alias histórico de troca de contatos

--- services/contact_exchange_pilot.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-"""Alias temporário para o auxiliar editorial de troca de contatos."""
-
-import sys
-from services import editorial_contact_exchange as _editorial_contact_exchange
-
-sys.modules[__name__] = _editorial_contact_exchange
