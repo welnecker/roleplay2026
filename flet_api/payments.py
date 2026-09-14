7179382e468f73f12c72bc56cb98ef09aaf6e108
welnecker <janiofdr@gmail.com>
2026-09-14 14:00:41 -0300

    Repair payment gateway PostgreSQL integration

--- flet_api/payments.py
+++ flet_api/payments.py
@@ -10,7 +10,7 @@ from billing.master_test import MasterTestPaymentService
 from billing.mercado_pago import MercadoPagoClient
 from billing.service import PixCheckoutService, read_secret
 from packages.loader import discover_packages
-from persistence.accounts import AccountUser\nfrom persistence.backend_config import POSTGRES_BACKEND, database_url, operational_backend\nfrom persistence.postgres_database import shared_postgres_database\nfrom persistence.postgres_narrative import PostgresStoryCreditRepository\nfrom persistence.postgres_payments import PostgresPaymentRepository
+from persistence.accounts import AccountUser\nfrom persistence.backend_config import POSTGRES_BACKEND, operational_backend\nfrom persistence.postgres_database import shared_postgres_database\nfrom persistence.postgres_narrative import PostgresStoryCreditRepository\nfrom persistence.postgres_payments import PostgresPaymentRepository
 from persistence.payments import GoogleSheetsPaymentRepository
 from persistence.spreadsheet_config import read_spreadsheet_ids
 from persistence.v2_google_sheets import GoogleSheetsStoryCreditRepository
@@ -42,8 +42,8 @@ class ServerPaymentGateway:
         self,
         *,
         accounts: Any,
-        payments: GoogleSheetsPaymentRepository,
-        credits: GoogleSheetsStoryCreditRepository,
+        payments: Any,
+        credits: Any,
         checkout: PixCheckoutService,
         master_test: MasterTestPaymentService,
         stories_root: Path,
