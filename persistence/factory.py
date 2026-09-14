325c4ecc1f59b9f442e2d237403644145a81e190
welnecker <janiofdr@gmail.com>
2026-09-14 13:59:10 -0300

    Fix PostgreSQL backend configuration lookup

--- persistence/factory.py
+++ persistence/factory.py
@@ -4,7 +4,7 @@ from queue import Empty, Queue
 from threading import Thread
 from typing import Any
 
-from persistence.backend_config import read_persistence_backend
+from persistence.backend_config import POSTGRES_BACKEND, database_url, operational_backend
 from persistence.editorial_runtime_v2 import EditorialGoogleSheetsV2RuntimeRepository
 from persistence.postgres_database import shared_postgres_database
 from persistence.postgres_runtime import PostgresV2RuntimeRepository
@@ -28,9 +28,8 @@ def _connect_runtime_repository(
 def build_google_sheets_repository(secrets: Any) -> Any | None:
     """Seleciona o runtime operacional, preservando Sheets como rollback seguro."""
 
-    backend = read_persistence_backend(secrets)
-    if backend.name == "postgres":
-        database = shared_postgres_database(backend.database_url)
+    if operational_backend(secrets) == POSTGRES_BACKEND:
+        database = shared_postgres_database(database_url(secrets))
         database.ensure_schema()
         return PostgresV2RuntimeRepository(database)
 
